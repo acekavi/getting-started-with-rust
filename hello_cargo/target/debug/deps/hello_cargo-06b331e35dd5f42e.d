4491955eb923edf567c0da7a5e8a8e41f6083705
acekavi <dfntlynotace@gmail.com>
2023-01-01 21:05:12 +0530

    Trying cargo - successfull

---
+++ hello_cargo/target/debug/deps/hello_cargo-06b331e35dd5f42e.d
@@ -0,0 +1,5 @@
+/home/acekavi/start_rust/hello_cargo/target/debug/deps/hello_cargo-06b331e35dd5f42e.rmeta: src/main.rs
+
+/home/acekavi/start_rust/hello_cargo/target/debug/deps/hello_cargo-06b331e35dd5f42e.d: src/main.rs
+
+src/main.rs:
