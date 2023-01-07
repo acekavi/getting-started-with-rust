2cb5e8a5a597780d6df0125ced3f3d66ce65d136
acekavi <dfntlynotace@gmail.com>
2023-01-07 11:26:45 +0530

    mutability

---
+++ variables/target/debug/deps/variables-be427bed55464bf3.d
@@ -0,0 +1,5 @@
+/home/acekavi/start_rust/variables/target/debug/deps/variables-be427bed55464bf3.rmeta: src/main.rs
+
+/home/acekavi/start_rust/variables/target/debug/deps/variables-be427bed55464bf3.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/deps/variables-c367c3c77ed8b46e.d
@@ -0,0 +1,5 @@
+/home/acekavi/start_rust/variables/target/debug/deps/variables-c367c3c77ed8b46e.rmeta: src/main.rs
+
+/home/acekavi/start_rust/variables/target/debug/deps/variables-c367c3c77ed8b46e.d: src/main.rs
+
+src/main.rs:
