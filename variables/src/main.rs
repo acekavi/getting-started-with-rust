b8054503ec49c65c92de28573c9a8a9dfc22a226
acekavi <dfntlynotace@gmail.com>
2023-01-07 12:52:16 +0530

    U can use the same variable with different datatypes using let without the mut. Kinda neat

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,12 @@ fn main() {
     println!("Value of x is {x}");
     x = 10;
     println!("Value of x is {x}");
+
+    const SEC_IN_H:u16 = 60 * 60;
+    println!("Seconds in an hour: {SEC_IN_H}");
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("number of spaces: {spaces}");
 }
