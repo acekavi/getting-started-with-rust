ff04324388e0be0b26610520bb5d0b566a0dd617
acekavi <dfntlynotace@gmail.com>
2023-01-07 10:25:25 +0530

    Mutability

---
+++ variables/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut x = 5;
+    println!("Value of x is {x}");
+    x = 10;
+    println!("Value of x is {x}");
+}
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/acekavi/start_rust/variables/target/debug/variables: /home/acekavi/start_rust/variables/src/main.rs
