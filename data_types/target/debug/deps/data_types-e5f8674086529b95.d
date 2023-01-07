8200a933efd4318fca36d4b52b4389999d4da5c0
acekavi <dfntlynotace@gmail.com>
2023-01-07 13:30:43 +0530

    Init

---
+++ data_types/target/debug/deps/data_types-e5f8674086529b95.d
@@ -0,0 +1,5 @@
+/home/acekavi/start_rust/data_types/target/debug/deps/data_types-e5f8674086529b95.rmeta: src/main.rs
+
+/home/acekavi/start_rust/data_types/target/debug/deps/data_types-e5f8674086529b95.d: src/main.rs
+
+src/main.rs:
