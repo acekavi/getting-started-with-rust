209c13ce43d38bde6c5ecfab35be0db1cff54571
acekavi <dfntlynotace@gmail.com>
2023-01-08 11:34:43 +0530

    Init

---
+++ functions/target/debug/deps/functions-aa771d07f2d58f8f.d
@@ -0,0 +1,5 @@
+/home/acekavi/start_rust/functions/target/debug/deps/functions-aa771d07f2d58f8f: src/main.rs
+
+/home/acekavi/start_rust/functions/target/debug/deps/functions-aa771d07f2d58f8f.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/acekavi/start_rust/functions/target/debug/functions: /home/acekavi/start_rust/functions/src/main.rs
