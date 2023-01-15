56933222cd17bcb22a2a79f7e8c02e374c9a8b09
acekavi <dfntlynotace@gmail.com>
2023-01-15 21:33:59 +0530

    Referencing is done using & added to the front of a variable

---
+++ references/target/debug/deps/references-5d6d086779d7b41a.d
@@ -0,0 +1,5 @@
+/home/acekavi/start_rust/references/target/debug/deps/references-5d6d086779d7b41a: src/main.rs
+
+/home/acekavi/start_rust/references/target/debug/deps/references-5d6d086779d7b41a.d: src/main.rs
+
+src/main.rs:
---
+++ references/target/debug/references.d
@@ -0,0 +1,1 @@
+/home/acekavi/start_rust/references/target/debug/references: /home/acekavi/start_rust/references/src/main.rs
