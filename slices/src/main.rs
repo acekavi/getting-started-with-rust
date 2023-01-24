cbe7f8345c7da38c1fb9bb088201ad56eb208de2
acekavi <dfntlynotace@gmail.com>
2023-01-24 19:09:35 +0530

    Slice different word

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let text = String::from("quick brown fox jump up the box");
+    let text = String::from("Quick brown fox jumps over the wall");
 
     println!("{}",first_word(&text));
 }
@@ -7,9 +7,18 @@ fn main() {
 fn first_word(input_text: &String) -> &str{
     let bytes = input_text.as_bytes();
 
+    let count:u8 = 4;
+    let mut start_index:usize = 0;
+    let mut num_of_spaces:u8 = 0;
+
     for(i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &input_text[0..i];
+        if item == b' '{
+            if num_of_spaces == (count -1) {
+                return &input_text[start_index..i];
+            }else{
+                start_index = i;
+            }
+            num_of_spaces += 1;
         }
     }
 
